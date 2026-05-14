bd6f86c16191a654d680f4793922c7de774c5beb
V Azevedo <viniciusazevedo1a@gmail.com>
2026-05-14 15:38:31 -0300

    Delete petshop directory

--- petshop/src/main/java/com/pedrodetonhe/petshop/Repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pedrodetonhe.petshop.Repository;
-
-import com.pedrodetonhe.petshop.Model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
