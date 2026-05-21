a9094a568297dcfd91c1bb1d889479f0ca31a14f
V Azevedo <viniciusazevedo1a@gmail.com>
2026-05-21 07:42:42 -0300

    Delete petshop directory

--- petshop/src/main/java/com/viniciusAzevedo/petshop/Repository/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.viniciusAzevedo.petshop.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.viniciusAzevedo.petshop.Model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
--- petshop/src/main/java/com/viniciusAzevedo/petshop/Repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.viniciusAzevedo.petshop.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.viniciusAzevedo.petshop.Model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
